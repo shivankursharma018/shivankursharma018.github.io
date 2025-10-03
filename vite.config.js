92623a2c603576a97c122442d5f254459902a33c
shivankursharma018 <shivankursharma018@gmail.com>
2025-10-03 17:06:20 +0530

    fix: correct vite base

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import { defineConfig } from 'vite';
 // https://vitejs.dev/config/
 
 export default defineConfig({
-  base: '/shivankursharma018.github.io/', // Replace with your repository name
+  base: '/', // Replace with your repository name
 })
