aea298dec6d2b2760929dba47cdff273d0f08991
shivankursharma018 <shivankursharma018@gmail.com>
2025-10-03 16:32:40 +0530

    adding ghpages deployment from npm

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,7 @@
 import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 
 export default defineConfig({
-  plugins: [react()],
   base: '/shivankursharma018.github.io/', // Replace with your repository name
 })
